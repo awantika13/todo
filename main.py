83cb11879c26fcd68f293bc682c885bcd5b61a3f
awantika <aawantika13@gail.com>
2023-07-28 17:40:46 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+# a=int(input("enter first no."))
+# b=int(input("enter second no."))
+# c=int(input("enter third no."))
+# d= a*b*c
+# print(" multiplication of 3 no",d)
+
+# n=100
+# print(id(n))
+
+# n=17898
+# print(id(n))
+
+# (x,y,z)= 50, 27.6,"lenevo"
+# print(x,y,z,x+10)
+
+
+# (x,y,z)=10,10,10
+# print(x,y,z)
+
+# result= 100//40
+# print (result)
+
+# country= 'India'
+# print(country[4])
+
+# cars=['BMW','nexa','kia']
+# print(cars)
+
+# x,y,z= cars
+# print(x)
+# print(y)
+# print(z)
+
+
+# x="simplilearn"
+# print( "welcome to "+ x)
+
+'''str1="python, programming"
+#print(str1[8])
+#print(str1[-8])
+l1=str1.split(",")
+print(l1)
+print(type(l1))
+'''
+
+'''tup1=('a','b','c')
+tup2=('d','e','f')
+tup1 +=tup2
+
+print(tup1)
+tup1 * 2
+print(tup1)
+'''
+
+''' import math
+print(math.pi)
+import random
+print(random.random())
+'''
+
+'''A= "INDIA"
+print(A)
+print(A[1:])
+print(A*7)
+print(A.replace('IA','xyz'))
+'''
+
+'''l1=[2.3,'america',4]
+l1.append('USA')
+print(l1)
+'''
+
+# print(0x12)
+
+'''list1 = ['apple', 'mango', 'banana', 'kiwi']
+print(list1)
+list2 = ['apple',[8, 4, 6], 'banana', 'orange']
+print(list2)
+list2.reverse()
+print(list2)
+'''
+'''put1=('apple','orange','banana','kiwi')
+print(put1)
+
+put2=('apple',)
+#print(type(put2))
+
+y= list(put1)
+y.append('guava')
+put1=tuple(y)
+tup3=('cherry',)
+put1+ =tup3
+print(put1)
+'''
---
+++ todo2.py
@@ -0,0 +1,69 @@
+from function import get_todos,write_todos
+
+
+
+while True:
+     user_action= input(" type add,edit,complete or show ")
+     user_action= user_action.strip()
+
+     if user_action.startswith("add"):
+         todo= user_action[4:]
+
+         todos= get_todos()
+
+         todos.append(todo+'\n')
+
+         write_todos(todos,"todos.txt")
+
+     elif user_action.startswith("show"):
+
+         todos = get_todos()
+
+         for index,item in enumerate(todos):
+
+             item=item.strip("\n")
+
+             row= f"{index +1}-{item}"
+
+             print(row)
+
+     elif user_action.startswith("edit"):
+
+         try:
+             imp = int(user_action[5:])
+             print(imp)
+             imp = imp-1
+
+             todos = get_todos()
+
+             todo_new = input(" enter the new todo")
+
+             todos[imp]= todo_new  +'\n'
+
+             write_todos(todos,"todos.txt")
+         except ValueError:
+             print("Your Command i not Valid")
+             continue
+
+     elif user_action.startswith("complete"):
+
+         try:
+             number = int(user_action[9:])
+
+             todos = get_todos()
+             index= number-1
+             todo_to_remove= todos[index].strip("\n")
+             todos.pop(index)
+
+             write_todos(todos,"todos.txt")
+
+             message=f"todo { todo_to_remove} was removed from the list"
+             print(message)
+         except IndexError:
+             print("Index with that number is not present")
+             continue
+
+     elif user_action.startswith("exit"):
+         break
+     else:
+         print(" command is invalid")
